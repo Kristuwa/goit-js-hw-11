f00a347378a6eb2aaffa2b7690ad283ab9acb6d3
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-30 01:08:27 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -1,56 +1,108 @@
 import './css/styles.css';
 import { ServiceApi } from './news-api';
 import Notiflix from 'notiflix';
-
-
-const DEBOUNCE_DELAY = 300;
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const newServiceApi = new ServiceApi();
 
 const refs = {
   form: document.querySelector('.search-form'),
-  button: document.querySelector('.load_more'),
+  button: document.querySelector('.load-more'),
   galleryList: document.querySelector('.gallery'),
 };
 
 refs.form.addEventListener('submit', onSearch);
+refs.button.addEventListener('click', onLoadMoreImg);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 function onSearch(e) {
   e.preventDefault();
+  clearGallery();
+
+  refs.button.classList.add('is-hidden');
 
   newServiceApi.query = e.currentTarget.elements.searchQuery.value;
-newServiceApi.resetPage();
-  newServiceApi.fetchAnimals();
+
+  newServiceApi.resetPage();
+  newServiceApi.fetchAnimals().then(({ hits, totalHits }) => {
+    if (hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+    refs.button.classList.remove('is-hidden');
+    return animalsMarkup(hits);
+  });
+}
+
+function onLoadMoreImg() {
+  newServiceApi.fetchAnimals().then(({ hits, totalHits }) => {
+    lightbox.refresh();
+    const maxPage = totalHits / hits.length;
+
+    if (maxPage <= newServiceApi.page) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      refs.button.classList.add('is-hidden');
+      return;
+    }
+    return animalsMarkup(hits);
+  });
 }
 
 function animalsMarkup(data) {
-refs.galleryList.insertAdjacentHTML('beforeend', galleryItem(data));
+  refs.galleryList.insertAdjacentHTML('beforeend', galleryItem(data));
 }
 
-function galleryItem({
-  webformatURL,
-  largeImageURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
-  return `<div class="photo-card">
-			<img src="${webformatURL}" alt="${tags}" loading="lazy" />
-			<div class="info">
-			  <p class="info-item">
-				 <b>Likes: ${likes}</b>
-			  </p>
-			  <p class="info-item">
-				 <b>Views: ${views}</b>
-			  </p>
-			  <p class="info-item">
-				 <b>Comments: ${comments}</b>
-			  </p>
-			  <p class="info-item">
-				 <b>Downloads: ${downloads}</b>
-			  </p>
-			</div>
-		 </div>`;
+function galleryItem(data) {
+  const markup = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+		  <a class="gallery-item" href='${largeImageURL}'>
+ <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height="214"/></a>
+ <div class="info">
+	<p class="info-item">
+	  <b>Likes:</b> ${likes}
+	</p>
+	<p class="info-item">
+	  <b>Views:</b> ${views}
+	</p>
+	<p class="info-item">
+	  <b>Comments:</b> ${comments}
+	</p>
+	<p class="info-item">
+	  <b>Downloads:</b> ${downloads}
+	</p>
+ </div>
+</div>`;
+      }
+    )
+    .join('');
+  return markup;
+}
+
+function clearGallery() {
+  refs.galleryList.innerHTML = '';
+}
+
+{
+  /* <a class="gallery-item" href='${largeImageURL}> */
 }
--- src/news-api.js
+++ src/news-api.js
@@ -1,7 +1,5 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
-const BASE_URL = 'https://pixabay.com/api';
-
 export class ServiceApi {
   constructor() {
     this.page = 1;
@@ -9,34 +7,30 @@ export class ServiceApi {
   }
 
   fetchAnimals() {
- const key = '30242343-f6d10ec55d07081d5dcce6a52';
-const url = `https://pixabay.com/api/?key=${key}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
-    return fetch(url).then(response => response.json()).then(data => {
-      this.incrementPage(); 
-    return ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    } = data); });
+    const key = '30242343-f6d10ec55d07081d5dcce6a52';
+    const url = `https://pixabay.com/api/?key=${key}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        console.log(data);
+        return data;
+      });
   }
 
-  incrementPage(){
-this.page += 1;
+  incrementPage() {
+    this.page += 1;
   }
 
-  resetPage(){
-    this.page=1;
+  resetPage() {
+    this.page = 1;
   }
 
   get query() {
     return this.searchQuery;
   }
 
- set query(newQuery) {
-    this.searchQuery=newQuery;
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
