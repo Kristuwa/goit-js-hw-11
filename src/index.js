95625c066e5ba42f6e344695c7fc998977eb33c7
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-29 11:26:46 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import './css/styles.css';
 // import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  button: document.querySelector('.load_more'),
+};
+
+refs.form.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const searchQuery = e.currentTarget.elements.searchQuery.value;
+
+  const key = '30242343-f6d10ec55d07081d5dcce6a52';
+
+  const url = `https://pixabay.com/api/?key=${key}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(console.log);
+}
